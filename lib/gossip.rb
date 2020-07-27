ba7fff004db305ca97144028e64a48bc3333b6e6
Maxime Pierron <maxime.pierron@outlook.com>
2020-07-27 17:16:26 +0200

    commentaires

--- lib/gossip.rb
+++ lib/gossip.rb
@@ -27,9 +27,9 @@ class Gossip
     def self.find(id)
         found_gossip = []
 
-        CSV.foreach("./db/gossip.csv").with_index do |row, i|
-            if i == (id.to_i - 1)
-                found_gossip << Gossip.new(row[0], row[1])
+        CSV.foreach("./db/gossip.csv").with_index do |row, i| #pour chaque élément de ce CSV, on travaille sur row et index
+            if i == (id.to_i - 1) #index commence de 0 alors que id commence à 1
+                found_gossip << Gossip.new(row[0], row[1]) #on stocke le gossip correspondant dans un array
             end
         end
 
@@ -40,14 +40,14 @@ class Gossip
         updated_gossips = []
 		CSV.foreach("./db/gossip.csv").with_index do |row, i|
 			if i == (id.to_i - 1)
-				updated_gossips << [author, content]
+				updated_gossips << [author, content] #si l'id correspond on stocke dans l'array la row modifiée
 			else
-				updated_gossips << [row[0], row[1]]
+				updated_gossips << [row[0], row[1]] #sinon on garde l'originale
 			end
         end
-        CSV.open("./db/gossip.csv", "w") do |csv| 
-		    updated_gossips.each do |row|
-		        csv << row
+        CSV.open("./db/gossip.csv", "w") do |csv| #on ouvre le csv et sur le fichier
+		    updated_gossips.each do |row| #pour chaque ligne de l'array
+		        csv << row #on injecte cette ligne dans le csv ce qui remplace les valeurs précédentes en gardant l'indexation
             end
         end
 
